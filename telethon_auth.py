eb305db48158c1deb17d90945036647db2a9d248
Stah2024 <stahcoin@gmail.com>
2025-10-13 12:48:26 +0300

    Update telethon_auth.py

--- telethon_auth.py
+++ telethon_auth.py
@@ -1,7 +1,1 @@
-from telethon.sync import TelegramClient
-
-api_id = 29950619
-api_hash = '85bcb9af365871ca4c8650a756b1ff78'
-
-client = TelegramClient('bot/session', api_id, api_hash)
-client.start() 
+ 
